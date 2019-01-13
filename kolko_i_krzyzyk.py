6decee5708b15af7ae2034a32776582a55f399ea
kamilb9 <kamilb9@gmail.com>
2019-01-13 12:53:36 +0100

    gra

--- kolko_i_krzyzyk.py
+++ kolko_i_krzyzyk.py
@@ -1,6 +1,9 @@
 def wyswietlPlansze(lista2D):
     for i in lista2D:
         for e in i:
-            print(i)
+            print(i,end="")
+        print()
 
 plansza=[["*","*","*",],["*","*","*",],["*","*","*",]]
+
+wyswietlPlansze(plansza)
