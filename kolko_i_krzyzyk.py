142d890e5e8b0074acc9c1f8b5cf5b33e8d1e7c9
kamilb9 <kamilb9@gmail.com>
2019-01-13 13:04:37 +0100

    gra

--- kolko_i_krzyzyk.py
+++ kolko_i_krzyzyk.py
@@ -1,11 +1,19 @@
 def wyswietlPlansze(lista2D):
+    print("",1,2,3)
+    nr_wiersza=1
     for i in lista2D:
+        print(nr_wiersza,end="")
         for e in i:
             print(e,end=" ")
+        nr_wiersza+=1
         print()
 
 graKrzyzyk=False
 start=input("X czy O: ")
+
+if start == "X":
+    graKrzyzyk=True
+
 
 plansza=[["*","*","*",],["*","*","*",],["*","*","*",]]
 
